9c54b8102666bc9e411498b216719d957363280c
syang0624 <steven.yang@uni.minerva.edu>
2025-01-18 17:21:20 +0900

    URL path digit control

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,9 +3,10 @@ import { see, edit, upload, deleteVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
+// have to put upload on top of the :id as it could be a valid id.
 videoRouter.get("/upload", upload);
-videoRouter.get("/:id", see);
-videoRouter.get("/:id/edit", edit);
-videoRouter.get("/:id/delete", deleteVideo);
+videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
 export default videoRouter;
