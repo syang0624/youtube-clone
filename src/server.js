8232f9fe730826b4ab803684f0e1f4f15503f7a4
syang0624 <steven.yang@uni.minerva.edu>
2025-01-20 17:43:01 +0700

    connect mongoDB

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import "./db";
+
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
@@ -12,6 +14,7 @@ const logger = morgan("dev");
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
