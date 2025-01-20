2aad51c04776828a7d5107b6e123c7d914036f5b
syang0624 <steven.yang@uni.minerva.edu>
2025-01-20 18:10:10 +0700

    Video Model schema done

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import "./db";
-
+import "./models/Video";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
@@ -21,7 +21,7 @@ app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
 const handleListening = () => {
-    console.log(`Server listening on port http://localhost:${PORT} 🚀`);
+    console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 };
 
 app.listen(PORT, handleListening);
