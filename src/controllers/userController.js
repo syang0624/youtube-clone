27fa10e48f25046b3cfca46464b308b56d9adf70
syang0624 <steven.yang@uni.minerva.edu>
2025-01-28 16:00:17 +0900

    create account and form validation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,35 @@
-export const join = (req, res) => res.send("join");
+import User from "../models/User";
+
+export const getJoin = (req, res) =>
+    res.render("join", { pageTitle: "Create Account" });
+export const postJoin = async (req, res) => {
+    const { name, username, email, password, password2, location } = req.body;
+    const pageTitle = "Join";
+    if (password !== password2) {
+        return res.render("join", {
+            pageTitle,
+            errorMessage: "Password does NOT match.",
+        });
+    }
+    const exists = await User.exists({
+        $or: [{ username: username }, { email: email }],
+    });
+    if (exists) {
+        return res.render("join", {
+            pageTitle,
+            errorMessage: "This username/email is already taken.",
+        });
+    }
+
+    await User.create({
+        name,
+        username,
+        email,
+        password,
+        location,
+    });
+    return res.redirect("/login");
+};
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
 export const login = (req, res) => res.send("Login");
