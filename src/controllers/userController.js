f5d1859a91252607983a1861120f730169314bf4
syang0624 <steven.yang@uni.minerva.edu>
2025-01-31 18:07:05 +0900

    github oauth account merge and logout feature

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -38,12 +38,14 @@ export const postJoin = async (req, res) => {
         });
     }
 };
+
 export const getLogin = (req, res) =>
     res.render("login", { pageTitle: "Login" });
+
 export const postLogin = async (req, res) => {
     const { username, password } = req.body;
     const pageTitle = "Login";
-    const user = await User.findOne({ username: username });
+    const user = await User.findOne({ username: username, socialOnly: false });
     if (!user) {
         return res.status(400).render("login", {
             pageTitle,
@@ -109,18 +111,36 @@ export const finishGithubLogin = async (req, res) => {
                 },
             })
         ).json();
-        const email = emailData.find(
+        const emailObj = emailData.find(
             (email) => email.primary === true && email.verified === true
         );
-        if (!email) {
+        if (!emailObj) {
             return res.redirect("/login");
         }
+        let user = await User.findOne({ email: emailObj.email });
+        if (!user) {
+            user = await User.create({
+                name: userData.name,
+                avatarUrl: userData.avatar_url,
+                username: userData.login,
+                email: emailObj.email,
+                password: "",
+                socialOnly: true,
+                location: userData.location,
+            });
+        }
+        req.session.loggedIn = true;
+        req.session.user = user;
+        return res.redirect("/");
     } else {
-        res.redirect("/login");
+        return res.redirect("/login");
     }
 };
 
 export const edit = (req, res) => res.send("Edit User");
-export const remove = (req, res) => res.send("Remove User");
-export const logout = (req, res) => res.send("LogOut");
+
+export const logout = (req, res) => {
+    req.session.destroy();
+    return res.redirect("/");
+};
 export const see = (req, res) => res.send("See User");
