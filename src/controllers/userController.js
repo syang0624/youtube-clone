c24f8ecc92388c3c15c6c9e9935dabb8817a20ee
syang0624 <steven.yang@uni.minerva.edu>
2025-02-02 16:48:34 +0900

    update profile

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -141,8 +141,26 @@ export const getEdit = (req, res) => {
     return res.render("edit-profile", { pageTitle: "Edit Profile" });
 };
 
-export const postEdit = (req, res) => {
-    return res.render("edit-profile");
+export const postEdit = async (req, res) => {
+    const {
+        session: {
+            user: { _id },
+        },
+        body: { name, email, username, location },
+    } = req;
+
+    const updatedUser = await User.findByIdAndUpdate(
+        _id,
+        {
+            name: name,
+            email: email,
+            username: username,
+            location: location,
+        },
+        { new: true }
+    );
+    req.session.user = updatedUser;
+    return res.redirect("/users/edit");
 };
 
 export const edit = (req, res) => res.send("Edit User");
