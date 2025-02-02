688831f00d2a4d5faaecd4f4b7895827f20ad994
syang0624 <steven.yang@uni.minerva.edu>
2025-02-02 12:42:36 +0900

    videoRouter protector update

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -7,14 +7,26 @@ import {
     postUpload,
     deleteVideo,
 } from "../controllers/videoController";
+import { protectorMiddleware } from "../middlewares";
 
 const videoRouter = express.Router();
 
 // have to put upload on top of the :id as it could be a valid id.
 
 videoRouter.get("/:id([0-9a-f]{24})", watch);
-videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
-videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
-videoRouter.route("/upload").get(getUpload).post(postUpload);
+videoRouter
+    .route("/:id([0-9a-f]{24})/edit")
+    .all(protectorMiddleware)
+    .get(getEdit)
+    .post(postEdit);
+videoRouter
+    .route("/:id([0-9a-f]{24})/delete")
+    .all(protectorMiddleware)
+    .get(deleteVideo);
+videoRouter
+    .route("/upload")
+    .all(protectorMiddleware)
+    .get(getUpload)
+    .post(postUpload);
 
 export default videoRouter;
