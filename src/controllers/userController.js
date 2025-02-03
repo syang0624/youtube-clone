fb6a87baa25e557377e7a721a1cf8994d0f2cf3d
syang0624 <steven.yang@uni.minerva.edu>
2025-02-03 13:39:34 +0900

    feature: upload avatar files

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -144,9 +144,10 @@ export const getEdit = (req, res) => {
 export const postEdit = async (req, res) => {
     const {
         session: {
-            user: { _id },
+            user: { _id, avatarUrl },
         },
         body: { name, email, username, location },
+        file,
     } = req;
 
     const currentUser = req.session.user;
@@ -171,6 +172,7 @@ export const postEdit = async (req, res) => {
     const updatedUser = await User.findByIdAndUpdate(
         _id,
         {
+            avatarUrl: file ? file.path : avatarUrl,
             name: name,
             email: email,
             username: username,
