dd229f321fb91f3329d5591214e6e3cf28012375
syang0624 <steven.yang@uni.minerva.edu>
2025-02-11 17:45:37 +0900

    feature: real-time comment

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,7 +1,20 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
-const handleSubmit = (event) => {
+const addComment = (text) => {
+    const videoComments = document.querySelector(".video__comments ul");
+    const newComment = document.createElement("li");
+    newComment.className = "video__comment";
+    const icon = document.createElement("i");
+    icon.className = "fas fa-comment";
+    const span = document.createElement("span");
+    span.innerText = `  ${text}`;
+    newComment.appendChild(icon);
+    newComment.appendChild(span);
+    videoComments.prepend(newComment);
+};
+
+const handleSubmit = async (event) => {
     event.preventDefault();
     const textarea = form.querySelector("textarea");
     const text = textarea.value;
@@ -9,12 +22,15 @@ const handleSubmit = (event) => {
     if (text == "") {
         return;
     }
-    fetch(`/api/videos/${videoId}/comment`, {
+    const { status } = await fetch(`/api/videos/${videoId}/comment`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ text: text }),
     });
     textarea.value = "";
+    if (status === 201) {
+        addComment(text);
+    }
 };
 
 if (form) {
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -10,8 +10,10 @@ export const home = async (req, res) => {
 };
 export const watch = async (req, res) => {
     const { id } = req.params;
-    const video = await Video.findById(id).populate("owner");
-
+    const video = await Video.findById(id)
+        .populate("owner")
+        .populate("comments");
+    console.log(video);
     if (!video) {
         return res.render("404", { pageTitle: "Video Not Found" });
     }
@@ -149,5 +151,7 @@ export const createComment = async (req, res) => {
         owner: user._id,
         video: id,
     });
+    video.comments.push(comment._id);
+    video.save();
     return res.sendStatus(201);
 };
