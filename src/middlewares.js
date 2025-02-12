2d7354f792fc6a7870139117942e8622696c6973
syang0624 <steven.yang@uni.minerva.edu>
2025-02-12 21:17:57 +0900

    draft: deploy?

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,4 +1,32 @@
 import multer from "multer";
+import { S3Client } from "@aws-sdk/client-s3";
+import multerS3 from "multer-s3";
+
+const s3Client = new S3Client({
+    region: "ap-northeast-2",
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+});
+
+const s3AvatarStorage = multerS3({
+    s3: s3Client,
+    bucket: "syang0624-youtube-clone",
+    acl: "public-read",
+    key: function (req, file, cb) {
+        cb(null, `avatars/${req.session.user._id}/${Date.now().toString()}`);
+    },
+});
+
+const s3VideoStorage = multerS3({
+    s3: s3Client,
+    bucket: "syang0624-youtube-clone",
+    acl: "public-read",
+    key: function (req, file, cb) {
+        cb(null, `videos/${req.session.user._id}/${Date.now().toString()}`);
+    },
+});
 
 export const localsMiddleware = (req, res, next) => {
     res.locals.loggedIn = Boolean(req.session.loggedIn);
@@ -26,15 +54,15 @@ export const publicOnlyMiddleware = (req, res, next) => {
 };
 
 export const avatarUpload = multer({
-    dest: "uploads/avatars/",
     limits: {
         fileSize: 3000000,
     },
+    storage: s3AvatarStorage,
 });
 
 export const videoUpload = multer({
-    dest: "uploads/videos",
     limits: {
         fileSize: 10000000,
     },
+    storage: s3VideoStorage,
 });
